46091e6d4ec5206d3729cab7f3b95489029c0c38
ushervin <eddyusherjeezy@gmail.com>
2024-09-09 21:52:35 +0200

    js assignment

---
+++ script.js
@@ -0,0 +1,107 @@
+let name = "Edward"
+let greeting = "hey there, welcome"
+console.log(greeting + " " + name )
+
+let total= 7 + 3
+console.log(total)
+
+let x = 10
+let y = 5
+let xy = x + y
+console.log(xy)
+
+let sentence = " I am lerning Javascript"
+sentence = " I am learning Python"
+console.log(sentence)
+
+let age1 = 30
+let age2 = 25
+let ageDifference = age1 - age2
+let ageDifference1 = Math.abs(ageDifference)
+console.log(ageDifference1)
+
+let temperatureCelsius = 25
+let fahrenheit = temperatureCelsius * 1.8 + 32
+console.log(fahrenheit)
+
+let num1 = "6"
+let num2 = 2
+let product =  Number(num1) * num2
+console.log(product)
+
+/*let favColor = prompt("whats your fav color: ")
+console.log("Your fav color is " + favColor)*/
+
+let newNum = "37"
+let newNum2 = Number(newNum)
+let newValue = newNum2 * 2
+console.log(newValue)
+
+let itsRaining = true
+let message = " it is currently raining: " + itsRaining
+console.log(message)
+
+let price = 100
+let discount = 30
+let discountPrice = (price * discount) / 100
+let finalPrice = price - discountPrice
+console.log(finalPrice)
+
+/*let age = prompt("how old are you: ")
+let freshage = Number(age)
+let newage = 10
+let totalage = freshage + newage
+console.log(totalage) */
+
+let quote = "No condition is permanent"
+let quotelength = quote.length
+console.log("The length of the quote is: " + quotelength)
+
+let firstname = " Kwame"
+let lastname = " Gyasi"
+console.log( firstname + " " + lastname)
+
+let numstring = "42"
+let newnumstring = Number(numstring)
+let actualnumber = newnumstring + 8
+console.log(actualnumber)
+
+let sx = 5 
+sx = sx + 3
+console.log(sx)
+
+let name1 = "John"
+let greeting1 = "Hello, " + name1
+console.log(greeting1)
+
+const num4 = "5"
+const num5 = 3
+console.log(num4 + num5)
+
+const num6 = 10
+const num7 = "5"
+const result = num6 + num7
+console.log(result)
+
+const price1 = "20"
+const quantity = 3
+const total1 = price * quantity
+console.log(total1)
+
+let message1 = "Hello, World!"
+console.log(message1.length)
+
+const bobbi = 50
+bobbi = 30
+console.log(bobbi)
+
+
+
+
+
+
+
+
+
+
+
